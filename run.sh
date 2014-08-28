8d429bb8c8f83243954ca3f1ab58bf1feeae859b
Johan Euphrosine <proppy@google.com>
2014-08-28 15:29:06 -0700

    run.sh: add support for GCP_ACCOUNT

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ GUNICORN_GRACEFUL_TIMEOUT=${GUNICORN_GRACEFUL_TIMEOUT:-3600}
 GUNICORN_SILENT_TIMEOUT=${GUNICORN_SILENT_TIMEOUT:-3600}
 
 USAGE="docker run -e GCS_BUCKET=yet-another-docker-bucket \
+[-e GCP_ACCOUNT='<YOUR_EMAIL>' ] \
 [-e BOTO_PATH='/.config/gcloud/legacy_credentials/<YOUR_EMAIL>/.boto'] \
 [-e GCP_OAUTH2_REFRESH_TOKEN=<refresh token>] \
 -p 5000:5000 \
@@ -30,12 +31,14 @@ fi
 
 # Look to see if something is mounted under the /.config dir
 if [[ -z "${BOTO_PATH}" ]]; then
-  BOTO_PATH=${BOTO_PATH:-$(find /.config | grep .boto | head -n 1)}
+  GCP_ACCOUNT=${GCP_ACCOUNT:-$(echo $(grep account /.config/gcloud/properties | cut -d = -f 2))}
+  BOTO_PATH=${BOTO_PATH:-$(find /.config/gcloud/legacy_credentials/$GCP_ACCOUNT/.boto)}
 fi
 
 if [[ -n "${BOTO_PATH}" ]]; then
   echo "Using credentials in ${BOTO_PATH}"
 else
+  # fallback on GCE auth
   wget -q --header='Metadata-Flavor: Google' \
     http://metadata.google.internal./computeMetadata/v1/instance/service-accounts/default/scopes \
     -O - | grep devstorage > /dev/null
@@ -46,8 +49,9 @@ else
   else
     echo "Boto credentials not found.  Either:"
     echo " - Set GCP_OAUTH2_REFRESH_TOKEN"
-    echo " - Mount credentials under /.config in the container"
-    echo " - Run in GCE with a service account configured for access"
+    echo " - Set BOTO_PATH"
+    echo " - Mount gcloud credentials under /.config in the container and optionally set GCP_ACCOUNT"
+    echo " - Run in GCE with a service account configured for devstorage access"
     echo
     echo "$USAGE"
     exit 1
