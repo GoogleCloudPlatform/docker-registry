32b9bb4ee2981a136b48c1e4dfbed0781a544af4
Johan Euphrosine <proppy@google.com>
2014-11-07 13:22:17 -0800

    run: generate separate server certs

--- run.sh
+++ run.sh
@@ -55,12 +55,17 @@ else
 fi
 
 if [ "${SECURE}" = "ssl" ]; then
-  ls /ssl/ssl.conf || cat <<EOF > /ssl/ssl.conf
+  set -x
+  if ! ls /ssl/ssl.conf; then
+      cat <<EOF > /ssl/ssl.conf
 [req]
 distinguished_name = req_distinguished_name
 [req_distinguished_name]
 [v3_ca]
 basicConstraints = CA:TRUE
+subjectAltName = @alt_names
+[v3_req]
+basicConstraints = CA:FALSE
 subjectAltName = @alt_names
 [alt_names]
 DNS.1 = localhost
@@ -68,8 +73,15 @@ DNS.1 = boot2docker.local
 IP.1 = 192.168.59.103
 IP.2 = 127.0.0.1
 EOF
-  ls /ssl/ca.{key,crt} || (openssl req -subj "/CN=${REGISTRY_COMMON_NAME}" -config /ssl/ssl.conf -extensions v3_ca -new -x509 -days 365 -newkey rsa:2048 -nodes -keyout /ssl/ca.key -out /ssl/ca.crt && mkdir -p /certs.d/${REGISTRY_COMMON_NAME} && cp /ssl/ca.crt /certs.d/${REGISTRY_COMMON_NAME}/)
-  ls /ssl/server.{key,cert} || (ln -s /ssl/ca.key /ssl/server.key && ln -s /ssl/ca.crt  /ssl/server.cert)
+  fi
+  if ! ls /ssl/ca.{key,crt}; then
+      echo 01 > /ssl/ca.srl
+      openssl req -subj "/CN=${REGISTRY_COMMON_NAME}" -config /ssl/ssl.conf -extensions v3_ca -new -x509 -days 365 -newkey rsa:2048 -nodes -keyout /ssl/ca.key -out /ssl/ca.crt
+      openssl req -subj "/CN=${REGISTRY_COMMON_NAME}" -config /ssl/ssl.conf -reqexts v3_req -new -newkey rsa:2048 -nodes -keyout /ssl/server.key -out /ssl/server.csr
+      openssl x509 -req -extfile /ssl/ssl.conf -extensions v3_req -days 365 -in /ssl/server.csr -CA /ssl/ca.crt -CAkey /ssl/ca.key -out /ssl/server.cert
+      mkdir -p /certs.d/${REGISTRY_COMMON_NAME}
+      cp /ssl/ca.crt /certs.d/${REGISTRY_COMMON_NAME}/
+  fi
   GUNICORN_OPTS="['--certfile','/ssl/server.cert','--keyfile','/ssl/server.key','--ca-certs','/ssl/ca.crt']"
 fi
 
